04d68aa4c35a067c3c12e2ecd63a3deb53b6036e
manila <manila@users.noreply.github.com>
2018-02-15 15:21:26 -0700

    Added 02.hs

---
+++ 02.hs
@@ -0,0 +1,7 @@
+fib :: Integer -> Integer
+fib 1 = 1
+fib 2 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+main :: IO ()
+main = putStrLn $ (show . sum . filter even . takeWhile (<4000000) . map (\n -> fib n)) $ [1..]
