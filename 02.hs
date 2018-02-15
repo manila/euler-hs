dbe4c076533bce80246e23ba3a7a59d5c98eb24f
manila <manila@users.noreply.github.com>
2018-02-15 15:27:19 -0700

    Removed Extra '$'

--- 02.hs
+++ 02.hs
@@ -4,4 +4,4 @@ fib 2 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
 main :: IO ()
-main = putStrLn $ (show . sum . filter even . takeWhile (<4000000) . map (\n -> fib n)) $ [1..]
+main = putStrLn $ (show . sum . filter even . takeWhile (<4000000) . map (\n -> fib n)) [1..]
